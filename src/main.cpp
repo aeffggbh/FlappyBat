8da392f1b9009b9fdd43eb88f152488cbdc396cc
Nicolas Leon <nicolasaleon05@gmail.com>
2024-11-01 13:03:55 -0300

    Update Objectives

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Game.h"
 
 //Falta configurar para que sea auto metan los archivos de res
+//Cambiar el nombre de enemy por obstacle
 //Chequear boton de pausa en gameplay (si se borra o no)
 
 int main()
