d18e7df0ee1f481d378d4a7090d9d028aafb9b10
Nicolas Leon <nicolasaleon05@gmail.com>
2024-11-05 11:32:03 -0300

    Add author and sprite creator

--- src/Scenes/Credits.cpp
+++ src/Scenes/Credits.cpp
@@ -6,9 +6,9 @@ namespace Credits
 {
 	static Texture2D background;
 
-	static int regularFontSize = 25;
-	static int titleFontSize = 70;
-	static int buttonFontSize = 40;
+	static int regularFontSize = 20;
+	static int titleFontSize = 50;
+	static int buttonFontSize = 30;
 
 	static float buttonWidth = 250;
 	static float buttonHeight = 60;
@@ -23,6 +23,15 @@ namespace Credits
 
 	Bton::Button returnToMenu;
 
+	//Texts
+	static Text::Text developerText;
+	static Text::Text spritesText;
+	static Text::Text spritesUrlText;
+
+
+	//Invisible button
+	static Bton::Button spritesUrlButton;
+
 
 	void Load()
 	{
@@ -33,11 +42,37 @@ namespace Credits
 		buttonCenterX = screenCenterX - buttonWidth / 2;
 
 		returnToMenu = Bton::Create("Return", buttonCenterX, screenHeight / 6 * 5, buttonWidth, buttonHeight);
+		
+		//Texts
+		{
+
+			developerText = Text::CreateText("Developed by: Nicolas Leon", titleFontSize, { screenCenterX, screenHeight / screenDivision * heightMultiplyer - 10 }, GREEN);
+			Text::SetTextLength(developerText);
+
+			heightMultiplyer += 2;
+
+			spritesText = Text::CreateText("Sprites by Caz Creates Games", regularFontSize, { screenCenterX, screenHeight / screenDivision * heightMultiplyer }, GREEN);
+			Text::SetTextLength(spritesText);
+
+
+			spritesUrlText = Text::CreateText("(caz-creates-games.itch.io/bat)", regularFontSize, { screenCenterX, screenHeight / screenDivision * heightMultiplyer }, GOLD);
+			Text::SetTextLength(spritesUrlText);
+
+			heightMultiplyer++;
+		}
+
+
+
+		//Invisible buttons
+		{
+			spritesUrlButton = Bton::Create("", spritesUrlText.pos.x, spritesUrlText.pos.y, static_cast<float>(spritesUrlText.length), static_cast<float>(regularFontSize));
+		}
 	}
 
 	void Update()
 	{
-		//Invisible buttons click check
+		if (Bton::IsButtonPressed(spritesUrlButton)) OpenURL("https://caz-creates-games.itch.io/bat");
+		
 	}
 
 	void Draw()
@@ -45,6 +80,10 @@ namespace Credits
 		ClearBackground(BLACK);
 		DrawTexture(background, 0, 0, WHITE);
 		Bton::Draw(returnToMenu, buttonFontSize);
+
+		Text::DrawCentered(developerText);
+
+		Text::DrawCentered(spritesText, spritesUrlText);
 	}
 
 
