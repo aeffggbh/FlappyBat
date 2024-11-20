be32179d250c3d08b4ddb976a395dfbfc34b5251
aeffggbh <genuinedistinctclearcapital@gmail.com>
2024-11-19 22:26:40 -0300

    Change the pause scene title font and color

--- src/Scenes/Pause.cpp
+++ src/Scenes/Pause.cpp
@@ -1,11 +1,13 @@
 #include "Pause.h"
 
 #include "Interface/ColorManager.h"
+#include "Interface/Text.h"
 
 namespace Pause
 {
 	Buttons::Button returnToMenu;
 	Buttons::Button continuePlaying;
+	Text::Text gamePaused;
 
 	static int fontSize = 40;
 	static int titleFontSize = fontSize * 2;
@@ -23,18 +25,17 @@ namespace Pause
 
 		returnToMenu = Buttons::Create("Go to menu", buttonCenterX - buttonWidth, static_cast<float>(screenHeight / 2), buttonWidth, buttonHeight);
 		continuePlaying = Buttons::Create("Continue", buttonCenterX + buttonWidth, static_cast<float>(screenHeight / 2), buttonWidth, buttonHeight);
+		gamePaused = Text::CreateText("Game Paused", titleFontSize,
+			{ static_cast<float>(GetScreenWidth() / 2), static_cast<float>(GetScreenHeight() / 6 - titleFontSize / 2) },
+			WHITE, Text::Fonts::subtitle);
+		Text::CenterText(gamePaused);
 	}
 
 	void Draw()
 	{
 		DrawRectangle(0, 0, GetScreenWidth(), GetScreenHeight(), ColorManager::GetColor(ColorManager::AlphaBlack));
 
-		const char* title = "Game Paused";
-		int textLength = MeasureText(title, titleFontSize);
-		int textX = static_cast<int> (screenCenterX - textLength / 2);
-		int textY = static_cast<int> (screenHeight / 6 - titleFontSize / 2);
-		DrawText(title, textX, textY, titleFontSize, RED);
-
+		Text::Draw(gamePaused);
 		Buttons::Draw(returnToMenu, fontSize);
 		Buttons::Draw(continuePlaying, fontSize);
 	}
